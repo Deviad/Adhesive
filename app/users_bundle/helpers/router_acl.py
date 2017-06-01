e21275022ab75300603694d149e7f47cc1ce56be
Davide Pugliese <dvd.pugliese@gmail.com>
2017-06-01 17:02:54 +0300

    created acl decorator draft; next step is creating the acl table

--- app/users_bundle/helpers/router_acl.py
+++ app/users_bundle/helpers/router_acl.py
@@ -12,17 +12,30 @@ app = Flask(__name__)
 app.config.from_object('config.DevelopmentConfig')
 db = SQLAlchemy(app)
 
+ADMINISTRATOR_ONLY = 0
+USER_ONLY = 1  # user is the current user
+ADMINISTRATOR_OR_USER = 2  # user is the current user
 
-def current_user_only(fn):
-    def ret_fn(*args, **kwargs):
-        current_user = CurrentUserHelper()
 
-        if current_user.id == int(request.args.get('id')):
-            return fn()
-        else:
-            # you can test this by changing status to whatever you like and then trying to connect to a route with
-            # a wrong user id e.g. http://localhost:5001/api/user/edit?id=24
-            response = json.jsonify({"status": "fail"})
-            response.status_code = 403
-            return response
-    return ret_fn
+def router_acl(user_type):
+    def router_acl_decorator(fn):
+        def func_wrapper(*args, **kwargs):
+            current_user = CurrentUserHelper()
+
+            if user_type == USER_ONLY:
+                if current_user.id == int(request.args.get('id')):
+                    return fn()
+                else:
+                    # you can test this by changing status to whatever you like and
+                    # then trying to connect to a route with
+                    # a wrong user id e.g. http://localhost:5001/api/user/edit?id=24
+                    response = json.jsonify({"status": "fail"})
+                    response.status_code = 403
+                    return response
+            # this is a fallback in case no valid type is provided
+            else:
+                response = json.jsonify({"status": "fail"})
+                response.status_code = 500
+                return response
+        return func_wrapper
+    return router_acl_decorator
