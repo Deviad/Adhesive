83f60801a77f82eed47ab559aa8c00ca867d204d
Davide Pugliese <dvd.pugliese@gmail.com>
2017-06-12 09:50:26 +0300

    corrected address model

--- theroot/users_bundle/models/address.py
+++ theroot/users_bundle/models/address.py
@@ -13,13 +13,14 @@ class Address(db.Model):
     user_info = db.relationship("UserInfo", secondary=address_user_table, back_populates="addresses")
 
     def __init__(self, address, country, geohash, the_zip=None):
-            self.address = address,
+            self.address_line = address,
             self.country = country,
             self.geohash = geohash
             self.zip = the_zip
 
     def __repr__(self):
-        return "<User (id='%r', address='%r', country='%r', geohash='%r', zip='%r')>" % (self.id, self.address, self.country, self.geohash, self.zip)
+        return "<User (id='%r', address_line='%r', country='%r', geohash='%r', zip='%r', user_info='%r')>" \
+               % (self.id, self.address_line, self.country, self.geohash, self.zip, self.user_info)
 
     def as_dict(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
